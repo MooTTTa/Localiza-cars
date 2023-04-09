ed57bc353b7f254b692a7394cbf2066e54fa3cdc
MM <mayconmotamendes@gmail.com>
2023-04-09 15:48:52 -0300

    add project spŕing

---
+++ src/main/java/com/localiza/Localizacars/LocalizaCarsApplication.java
@@ -0,0 +1,13 @@
+package com.localiza.Localizacars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalizaCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalizaCarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/localiza/Localizacars/LocalizaCarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localiza.Localizacars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalizaCarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
